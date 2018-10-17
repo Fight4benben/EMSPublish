6b3cb325d3386eeb8f848da5888f124a17e41d28
张奔 <zhangbenclass3@126.com>
2018-10-17 08:37:45 +0800

    整体提交，修改报警

--- app/js/Alarm/OutOfWork.js
+++ app/js/Alarm/OutOfWork.js
@@ -6,7 +6,7 @@ var OutOfWork = (function(){
 			initDateTime();
 			initButton();
 
-			var url = "/api/AlarmDeviceOverLimit";
+			var url = "/api/AlarmDeviceFreeTime";
 
 			getDataFromServer(url,"");
 		};
@@ -17,10 +17,10 @@ var OutOfWork = (function(){
 
 		function initButton(){
 			$("#Load").click(function(event) {
-				var url="/api/AlarmDeviceOverLimit";
+				var url="/api/AlarmDeviceFreeTime";
 
 				var params = "buildId="+$("#buildinglist").val()+"&date="+
-				$("#daycalendarBox").val()+"&type=Limit";
+				$("#daycalendarBox").val();
 
 				getDataFromServer(url,params);
 			});
@@ -66,18 +66,34 @@ var OutOfWork = (function(){
 			var columns=[
 				{field:'name',title:'名称',width:'250px'},
 				{field:'timePeriod',title:'非工作时间段'},
+				{field:'time',title:'时间'},
 				{field:'value',title:'能耗值'},
-				{field:'limitValue',title:'限定报警值'},
-				{field:'diffValue',title:'差值',cellStyle:function(value,row,index){
+				//{field:'limitValue',title:'限定报警值'},
+				// {field:'diffValue',title:'差值',cellStyle:function(value,row,index){
+				// 	if(row.limitValue >=0){
+				// 		var rate = row.diffValue/row.limitValue;
 
-					if(row.limitValue!=0){
-						var rate = row.diffValue/row.limitValue;
-						if(rate>0 && rate<=0.5)
+				// 		if(rate>0 && rate<=0.5)
+				// 			return {classes:'warning'}
+				// 		else if(rate>0.5)
+				// 			return {classes:'danger'}
+				// 	}else{
+				// 		return
+				// 	}
+				// }},
+				{field:'rate',title:'百分比'/*,cellStyle:function(value,row,index){
+					if(row.rate !== '-'){
+						var rate = parseFloat(row.rate.replace('%',''));
+
+						if(rate>0 && rate<=50)
 							return {classes:'warning'}
-						else if(rate>0.5)
+						else if(rate>50)
 							return {classes:'danger'}
+
+					}else{
+						return {classes:''}
 					}
-				}}
+				}*/}
 			];
 
 			var rows = [];
@@ -86,9 +102,15 @@ var OutOfWork = (function(){
 				var row={};
 				row.name = val.name;
 				row.value = val.value;
+				row.time = val.time;
 				row.timePeriod = val.timePeriod;
 				row.limitValue = val.limitValue;
-				row.diffValue = val.diffValue;
+				//row.diffValue = val.diffValue;
+
+				if(val.limitValue>0){
+					row.rate = ((val.value/val.limitValue)*100).toFixed(2) + '%';
+				}else
+					row.rate = '-';
 
 				rows.push(row);
 			});
@@ -97,7 +119,19 @@ var OutOfWork = (function(){
 			$("#alarmTable").html('<table></table>');
 			$("#alarmTable>table").attr('data-height',height);
 
-			EMS.DOM.showTable($("#alarmTable>table"),columns,rows,{striped:true,classes:'table table-border'});
+			EMS.DOM.showTable($("#alarmTable>table"),columns,rows,{striped:true,classes:'table table-border',
+				rowStyle:function(row,index){
+					 if(row.rate=="-")
+					 	return  {classes:''};
+
+					var rate = parseFloat(row.rate.substring(0,row.rate.length-1));
+					var style={};
+
+					if(rate>50)
+						style={classes:'danger'}//style={css:{'background-color':'#FF0000','color':'white'}};
+					
+		             return style;
+				}});
 
 			$("table td").css('font-size','15px');
 		}
